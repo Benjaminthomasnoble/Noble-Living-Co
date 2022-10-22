4daef687f5e5ff831e949e13e6a3cdd862a74ebd
Ben Noble <benjamin4504@gmail.com>
2022-10-21 20:52:46 -0400

    comments out current irrelevant code and removes scrollbar

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,50 +1,22 @@
-// // API Section
-
-// Axios Fetch Example
-// axios.get("https://swapi.dev/api/people/1/")
-//     .then(res => {
-//         console.log("RESPONSE: ", res);
-//     })
-//     .catch((e) => {
-//         console.log("ERROR ", e);
-//     });
-
-// Axios Fetch Example_REFACTORED
-// const getStarWarsPerson = async () => {
-//     const res = await axios.get("https://swapi.dev/api/people/1/")
-//     console.log(res.data)
-// };
-
-// getStarWarsPerson();
-
-// Axios Fetch Example_REFACTORED WITH ID
-// const getStarWarsPerson = async (id) => {
-//     const res = await axios.get(`https://swapi.dev/api/people/${id}/`)
-//     console.log(res.data)
-// };
-// getStarWarsPerson(5);
-
-
-
 // WP API
-const wpEndPoint = 'http://wdb.waj.mybluehost.me/wp-json'; //this is currently a dummy site to be replaced by noblelivingco.com
-const wpAPI = {
-    settings: `${wpEndPoint}/wp/v2/settings`,
-    posts: `${wpEndPoint}/wp/v2/posts`,
-    pages: `${wpEndPoint}/wp/v2/pages`,
-    postTypes: `${wpEndPoint}/wp/v2/types`,
-    acf: `${wpEndPoint}/acf/v3/posts`
-}
+// const wpEndPoint = 'http://wdb.waj.mybluehost.me/wp-json'; //this is currently a dummy site to be replaced by noblelivingco.com
+// const wpAPI = {
+//     settings: `${wpEndPoint}/wp/v2/settings`,
+//     posts: `${wpEndPoint}/wp/v2/posts`,
+//     pages: `${wpEndPoint}/wp/v2/pages`,
+//     postTypes: `${wpEndPoint}/wp/v2/types`,
+//     acf: `${wpEndPoint}/acf/v3/posts`
+// }
 
-const getWpPosts = async () => {
-    const res = await axios.get(wpAPI.posts)
-    const postData = res.data;
-    console.log(postData);
-    for (let post of postData) {
-        console.log(post.title.rendered);
-    }
-};
-getWpPosts();
+// const getWpPosts = async () => {
+//     const res = await axios.get(wpAPI.posts)
+//     const postData = res.data;
+//     console.log(postData);
+//     for (let post of postData) {
+//         console.log(post.title.rendered);
+//     }
+// };
+// getWpPosts();
 
 
 
